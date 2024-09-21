d496eb3980e281080c161e4983f9d650193c3d7a
Nicolas Romana <nicolas@ribere.fr>
2024-09-21 21:52:59 +0200

    fix

--- components/pentair_intelliflo/__init__.py
+++ components/pentair_intelliflo/__init__.py
@@ -3,29 +3,32 @@ import esphome.config_validation as cv
 from esphome.const import CONF_ID
 from esphome.components import uart
 
-DEPENDENCIES = ["uart"]
-AUTO_LOAD = ["binary_sensor", "text_sensor", "sensor", "switch", "output"]
 MULTI_CONF = True
+
+DEPENDENCIES = ["uart"]
+#AUTO_LOAD = ["binary_sensor", "text_sensor", "sensor", "switch", "output"]
 
 CONF_INTELLIFLO_ID = "intelliflo_id"
 
 intelliflo_ns = cg.esphome_ns.namespace("intelliflo")
+
 IntellifloComponent = intelliflo_ns.class_("Intelliflo", cg.Component)
 
-INTELLIFLO_COMPONENT_SCHEMA = cv.Schema(
+INTELLIFLO_CHILD_SCHEMA = cv.Schema(
     {
-        cv.Required(CONF_INTELLIFLO_ID): cv.use_id(IntellifloComponent),
+        cv.GenerateID(CONF_INTELLIFLO_ID): cv.use_id(IntellifloComponent),
     }
 )
 
-CONFIG_SCHEMA = cv.All(
-    cv.Schema({cv.GenerateID(): cv.declare_id(IntellifloComponent)})
-    .extend(cv.polling_component_schema("30s"))
-    .extend(uart.UART_DEVICE_SCHEMA)
-)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(IntellifloComponent),
+    }
+).extend(uart.UART_DEVICE_SCHEMA).extend(cv.polling_component_schema("30s"))
+    
 
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield uart.register_uart_device(var, config)
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
